085f0c67a6342ef2cc06c8f8fe6c646a3bfa91de
Mike Flintrop <mike.flintrop@gmail.com>
2022-06-06 11:30:12 -0500

    finished part 2 but still has issues

--- budget.js
+++ budget.js
@@ -5,11 +5,37 @@ let purchases = [];
 
 function addPurchase(){
     let newPurchase = {
-        name: $( 'purchaseNameIn' ).val(),
-        price: $( 'purchasePriceIn' ).val()
+        name: $( '#purchaseNameIn' ).val(),
+        price: $( '#purchasePriceIn' ).val()
     }
-    $( 'purchaseNameIn' ).val('');
-    $( 'purchasePriceIn' ).val('')
+    $( '#purchaseNameIn' ).val( '' );
+    $( '#purchasePriceIn' ).val( '' );
+
+    calculateRemainingBudget( );
+    displayPurchases();
+}
+
+function calculateRemainingBudget(){
+    console.log( 'in calculateRemainingBudget' );
+    let totalPrice = 0;
+    for (let i = 0; i < purchases.length; i++) {
+        totalPrice += Number(purchases[i].price);
+    }
+    console.log( 'totalPrice:', totalPrice);
+
+    const remainingBudget = budget - totalPrice;
+    let el = $( '#remainingBudgetOut' );
+    el.empty();
+    el.append( remainingBudget );
+}
+
+function displayPurchases(){
+    console.log( 'in displayPurchases' );
+    let el = $( '#purchasesOut' );
+    el.empty();
+    for ( purchase of purchases ) {
+        el.append( '<li>' + purchase.name + ': $' + purchase.price + '</li>' )
+    } 
 }
 
 function readyNow(){
@@ -19,5 +45,5 @@ function readyNow(){
    el.append( budget );
    //handle click event
    $( '#addPurchaseButton' ).on( 'click', addPurchase );
+   calculateRemainingBudget();
 }
-
